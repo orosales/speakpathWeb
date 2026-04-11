dfdf13368501f33f25d98c343e93460ac198750a
Omar Rosales <orosales.qast@gmail.com>
2026-04-10 21:01:45 -0300

    Improve google search and ai search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://speakpath.dev',
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
 });
